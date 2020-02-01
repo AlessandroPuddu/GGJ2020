b2cbd88d9bcf91346d276dc8b28e76c8e277f87a
Alessandro Ballerini <alessandroballerini95@gmail.com>
2020-02-01 19:27:51 +0100

    Conveyor movement done

---
+++ Assets/Scripts/CylinderRotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderRotation : MonoBehaviour
+{
+    void Update()
+    {
+        if (GameManager.Instance.IsConveyorBeltMoving())
+        {
+            Debug.Log("quelporconediddio");
+            transform.Rotate(new Vector3(0,0,100f) * Time.deltaTime, Space.Self);
+        }
+    }
+}
