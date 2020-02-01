e4d44bab3c81fff78e7bfd877a0ff808b3ebae53
AlessandroPuddu <40497778+AlessandroPuddu@users.noreply.github.com>
2020-02-01 14:43:22 +0100

    grab work in progress

--- Assets/Scripts/MasterAnchorCollisions.cs
+++ Assets/Scripts/MasterAnchorCollisions.cs
@@ -7,6 +7,11 @@ public class MasterAnchorCollisions : MonoBehaviour
     [SerializeField]
     private GameObject inactiveChild;
 
+    [SerializeField]
+    private GameObject rightPart;
+
+    // TODO - On trigger stay
+    // hiltight this to give feedback
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "SnapSlave")
@@ -18,12 +23,21 @@ public class MasterAnchorCollisions : MonoBehaviour
             {
                 if (!oVR.isGrabbed)
                 {
-                    Destroy(other.transform.root.gameObject);
-
+                    //oVR.grabbedBy.ForceRelease(oVR);
+                    // TODO - maybe destroy ovr.go?
+                    Destroy(oVR.gameObject);
+                    
                     // TODO - Check if this is already active?
+                    // TODO - Create a copy and then set active?
                     inactiveChild.SetActive(true);
+
+                    //GameEvents.RaiseOnRightPartPlaced();
                 }
             }
         }
+    }
+
+    public void AssignRightPart(GameObject part) { 
+        this.rightPart = part;
     }
 }
---
+++ Assets/Scripts/WrongPartBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(OVRGrabbable))]
+[RequireComponent(typeof(Rigidbody))]
+public class WrongPartBehaviour : MonoBehaviour
+{
+    private OVRGrabbable oVR;
+    [SerializeField]
+    private Rigidbody rb;
+    [SerializeField]
+    private BoxCollider bc;
+
+    private void Awake()
+    {
+        oVR = this.GetComponent<OVRGrabbable>();
+        //rb = this.transform.root.gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        //if (rb.useGravity) { 
+        //    Destroy(this);    
+        //}
+
+        if (oVR.isGrabbed && !rb.useGravity) {
+            // If I grab the wrong part
+            // I have to reenable its 
+            // rigidbody
+            bc.isTrigger = false;
+            rb.isKinematic = false;
+            rb.useGravity = true;
+        }
+    }
+}
