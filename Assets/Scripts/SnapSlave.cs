199a375ede3081241abd670615fd55ca3934b8db
AlessandroPuddu <40497778+AlessandroPuddu@users.noreply.github.com>
2020-02-01 12:32:32 +0100

    Snap maybe working

---
+++ Assets/Scripts/SnapSlave.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SnapSlave : MonoBehaviour
+{
+    GameObject text;
+
+    private void Start()
+    {
+        text = GameObject.FindGameObjectWithTag("DebugText");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!this.gameObject.transform.root.GetComponent<OVRGrabbable>().isGrabbed) 
+        {
+            
+            if (other.gameObject.tag == "SnapMaster")
+            {
+                //text.GetComponent<Text>().text += this.gameObject.transform.root.name + " is grabbed and is colliding with " + other.gameObject.name;
+                //text.GetComponent<Text>().text += "SnapMaster";
+
+                if (OVRInput.GetUp(OVRInput.Button.SecondaryHandTrigger) || OVRInput.GetUp(OVRInput.Button.PrimaryHandTrigger))
+                {
+                    this.transform.root.position = other.transform.position;
+                    this.transform.root.gameObject.GetComponent<Rigidbody>().useGravity = false;
+                }
+            }
+        }
+        
+    }
+}
