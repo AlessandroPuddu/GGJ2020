fe14118ff2ab331c417f61b5ef1285a89b5c7209
andreacarrarini <andrea.carrarini.18@gmail.com>
2020-02-01 11:04:03 +0100

    starting spawner

---
+++ Assets/Oculus/VR/Scripts/GameMiscScripts/BodyPartsSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyPartsSpawner : MonoBehaviour
+{
+    private GameObject arm;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
