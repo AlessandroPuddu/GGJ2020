4e98dd9cb4ee886ced47b0459e6eb3e03dbf3473
Alessandro Ballerini <alessandroballerini95@gmail.com>
2020-02-01 13:12:42 +0100

    Working on puppet movement

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> puppetsPrefabs;
+    public Transform puppetOrigin;
+    public Transform puppetStopPoint;
+    public Transform puppetDeadEnd;
+
+    private int level;
+    private GameObject currentPuppet;
+
+    private void Start()
+    {
+        level = 0;
+    }
+
+    public void NextPuppetButton()
+    {
+        NextLevel();
+        currentPuppet = Instantiate(puppetsPrefabs[0], puppetOrigin.position, puppetOrigin.rotation);
+        currentPuppet.GetComponent<PuppetMovement>().SetEndpoint(puppetStopPoint);
+    }
+
+    public void PuppetDoneButton()
+    {
+        currentPuppet.GetComponent<PuppetMovement>().SetEndpoint(puppetDeadEnd);
+    }
+
+    private void NextLevel()
+    {
+        level += 1;
+    }
+}
---
+++ Assets/Scripts/PuppetMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuppetMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Transform endPoint;
+    private bool run;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (endPoint && run == true)
+        {
+            if (Vector3.Distance(transform.position, endPoint.position) > 0.005f)
+            {
+                transform.Translate(Vector3.right * Time.deltaTime);
+            } else
+            {
+                run = false;
+            }
+        }
+    }
+
+    public void SetEndpoint(Transform endpoint)
+    {
+        this.endPoint = endpoint;
+        run = true;
+    }
+}
