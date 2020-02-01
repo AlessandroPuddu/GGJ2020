169947f05642d8ca502df707cafc66a1198b26d5
andreacarrarini <andrea.carrarini.18@gmail.com>
2020-02-01 16:13:20 +0100

    puppets prefabs created

--- Assets/Scripts/GameMiscScripts/Puppet.cs
+++ Assets/Scripts/GameMiscScripts/Puppet.cs
@@ -4,23 +4,45 @@ using UnityEngine;
 
 public class Puppet : MonoBehaviour
 {
-    private bool hasCrutch;
-    private bool hasChestDirty;
-    //private bool hasCrutch;
-    //private bool hasCrutch;
-    //private bool hasCrutch;
-    //private bool hasCrutch;
-    //private bool hasCrutch;
-    //private bool hasCrutch;
-    //private bool hasCrutch;
+    public bool hasCrutch;
+    public bool hasChestDirty;
+    public bool hasRightArmNotWhite;
+    public bool hasLeftArmNotWhite;
+    public bool hasRightLegNotWhite;
+    public bool hasLeftLegNotWhite;
+    public bool hasHeadNotWhite;
+    public bool hasRightArmBroken;
+    public bool hasLeftArmBroken;
+    public bool hasRightLegBroken;
+    public bool hasLeftLegBroken;
+    public bool hasHeadBroken;
+    public bool hasBlood;
+    public bool hasSyringe;
+    public bool hasClownNose;
+    public bool hasClownHair;
+    public bool hasHappyFace;
+    public bool hasChild;
+    public bool hasJewishChest;
+    public bool hasJewishRightArm;
+    public bool hasJewishLeftArm;
+    public bool hasJewishRightLeg;
+    public bool hasJewishLeftLeg;
 
     void Start()
     {
-        
+
     }
 
-    void Update()
+    public bool CheckAllRight()
     {
-        
+        if ( !hasCrutch && !hasChestDirty && !hasRightArmNotWhite && !hasLeftArmNotWhite && !hasRightLegNotWhite && !hasLeftLegNotWhite &&
+            !hasHeadNotWhite && !hasRightArmBroken && !hasLeftArmBroken && !hasRightLegBroken && !hasLeftLegBroken && !hasHeadBroken &&
+            !hasBlood && !hasSyringe && !hasClownNose && !hasClownHair && !hasHappyFace && !hasChild && !hasJewishChest && !hasJewishRightArm &&
+            !hasJewishLeftArm && !hasJewishRightLeg && !hasJewishLeftLeg )
+        {
+            return true;
+        }
+        return false;
     }
+
 }
