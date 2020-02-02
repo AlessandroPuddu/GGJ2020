a52181b1617ff83ffc9dab6eb53f44f0a249d566
Friudorian <falermarco@gmail.com>
2020-02-02 04:18:54 +0100

    burn

--- Assets/Scripts/Hellfire.cs
+++ Assets/Scripts/Hellfire.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class Hellfire : MonoBehaviour
 {
+
+    private AudioSource audioSource;
+    public void Awake()
+    {
+       audioSource=  GetComponent<AudioSource>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "ToDestroy")
         {
             Destroy(other.gameObject);
+            audioSource.clip = SoundManager.Instance.gimmeBurn();
+            audioSource.Play();
         }
     }
 }
