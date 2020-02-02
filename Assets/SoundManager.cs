849f4baa3baef6396d375d47a3c36946cd1d9560
Friudorian <falermarco@gmail.com>
2020-02-02 01:26:56 +0100

    SoundManager and sounds scattered

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager _instance;
+    public static SoundManager Instance { get { return _instance; } }
+
+    public List<AudioSource> cilindri;
+
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+
+
+    }
+
+    public void PlayRullo(bool play)
+    {
+        if (!play)
+        {
+            foreach (AudioSource a in cilindri)
+            {
+                a.Stop();
+            }
+        }
+        else
+        {
+            foreach (AudioSource a in cilindri)
+            {
+                if (a.isPlaying) return;
+                a.Play();
+            }
+        }
+    }
+}
