87469faa00ac6be6785f69e8bf3782c61be20b87
Friudorian <falermarco@gmail.com>
2020-02-02 09:43:56 +0100

    prto

---
+++ Assets/helpOrientation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helpOrientation : MonoBehaviour
+{
+
+    public OVRGrabbable ovrG;
+    // Update is called once per frame
+
+    private void Start()
+    {
+        ovrG = GetComponent<OVRGrabbable>();
+    }
+    void Update()
+    {
+        if (ovrG.snapOrientation)
+        {
+
+        }
+    }
+}
